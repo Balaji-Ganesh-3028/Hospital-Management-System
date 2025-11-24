47fffeebc18f4025b5506feb90bdf3ba92d1b9ca
Balaji Ganesh 3028 <balaji.ganesan@kanini.com>
2025-11-24 16:42:30 +0530

    Dashboard SP - Added Appointment's status count for the grapical representation

--- DB/dashboard_details_sp.sql
+++ DB/dashboard_details_sp.sql
@@ -5,24 +5,66 @@ BEGIN
 
   -- Dashboard Summary
   SELECT
+    -- PATIENT COUNTS
     (SELECT COUNT(Id)
     FROM PatientDetails) AS PatientCount,
+
+    -- DOCTOR COUNTS
     (SELECT COUNT(ID)
     FROM DoctorDetails) AS DoctorCount,
+
+    -- APPOINTMENT COUNTS
     (SELECT COUNT(ID)
     FROM AppointmentDirectory) AS AppointmentCount,
+
+    -- TOTAL USER COUNTS
     (SELECT COUNT(Id)
     FROM UserDirectory) AS TotalUserCount,
+
+    -- TODAY'S PATIENTS ADDED
     (SELECT COUNT(Id)
     FROM PatientDetails
     WHERE CAST(CreatedAt AS DATE) = CAST(GETDATE() AS DATE)) AS PatientAddedToday,
+
+    -- TODAY'S DOCOTRS ADDED
     (SELECT COUNT(ID)
     FROM DoctorDetails
     WHERE CAST(CreatedAt AS DATE) = CAST(GETDATE() AS DATE)) AS DoctorAddedToday,
+
+    -- TODAY'S APPOINTMENTS ADDED
     (SELECT COUNT(ID)
     FROM AppointmentDirectory
     WHERE CAST(CreatedAt AS DATE) = CAST(GETDATE() AS DATE))
-    AS TotalAppointmentsAddedToday;
+    AS TotalAppointmentsAddedToday,
+
+    -- TODAY'S APPOINTMENT STATUS COUNTS
+    -- COMPLETED APPOINTMENTS
+    (SELECT COUNT(ID)
+    FROM AppointmentDirectory
+    WHERE CurrentStatus = (SELECT Id
+      FROM MasterData
+      WHERE Value = 'Completed' AND Category = 'appointment_status') AND CAST(AppointmentDate AS DATE) = CAST(GETDATE() AS DATE)) AS CompletedAppointmentsToday,
+
+    -- SCHEDULED APPOINTMENTS
+    (SELECT COUNT(ID)
+    FROM AppointmentDirectory
+    WHERE CurrentStatus = (SELECT Id
+      FROM MasterData
+      WHERE Value = 'Scheduled' AND Category = 'appointment_status') AND CAST(AppointmentDate AS DATE) = CAST(GETDATE() AS DATE)) AS ScheduledAppointmentsToday,
+
+    -- CANCELLED APPOINTMENTS
+    (SELECT COUNT(ID)
+    FROM AppointmentDirectory
+    WHERE CurrentStatus = (SELECT Id
+      FROM MasterData
+      WHERE Value = 'Cancelled' AND Category = 'appointment_status') AND CAST(AppointmentDate AS DATE) = CAST(GETDATE() AS DATE)) AS CancelledAppointmentsToday,
+
+    -- RESCHEDULED APPOINTMENTS
+    (SELECT COUNT(ID)
+    FROM AppointmentDirectory
+    WHERE CurrentStatus = (SELECT Id
+      FROM MasterData
+      WHERE Value = 'Rescheduled' AND Category = 'appointment_status') AND CAST(AppointmentDate AS DATE) = CAST(GETDATE() AS DATE)) AS ReScheduledAppointmentsToday;
 END
 
 
